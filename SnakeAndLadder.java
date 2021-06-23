3e85b409630b5ce498064116dc74bf7ef6694936
Pavan-Saketh-Mandha <84276275+Pavan-Saketh-Mandha@users.noreply.github.com>
2021-06-23 17:09:47 +0530

    Update SnakeAndLadder.java
    Usecase 3

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,15 +9,46 @@
  */
 
 import java.util.*;
-public class SnakeAndLadderUC2
+public class SnakeAndLadder
 {
 	//System.out.println();
 	public static void main(String[] args) 
 	{
 		int start_position = 0;
+		int player_position = 0;
+		int countDice = 0;
 		System.out.println(" WELCOME - Snake And Ladder - SINGLE player");
 		System.out.println("Player is at Start position: "  + start_position);
 		int Dice = (int)((Math.random() * 6) + 1);
 		System.out.println("Output after rolling Dice: " + Dice);
+		int option = (int) (Math.random() * 3);
+		if (option == 1)//Ladder
+		{
+			System.out.println("Player is at position: " + player_position);
+			player_position += Dice;
+			System.out.println("LADDER - Move " + Dice + " positions up: " + player_position);
+			countDice++;
+		}
+		else if (option == 2)//Snake
+		{
+			System.out.println("Player is at position: " + player_position);
+			player_position -= Dice;
+			if (player_position < 0)
+			{
+				System.out.println("SNAKE - Move " + Dice + " positions down to: " + start_position);
+			}
+			else {
+			System.out.println("SNAKE - Move " + Dice + " positions down to: " + player_position);
+			}
+			countDice++;
+		}
+		else
+        {
+			System.out.println("Player is at position: " + player_position);
+			System.out.println("NO-PlAY - Stay in your current position: " +  player_position);
+			countDice++;	
+		}
+
 	}
 }
+
