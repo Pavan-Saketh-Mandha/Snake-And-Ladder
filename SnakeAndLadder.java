267d87948768e4b41454a7a3e4c3b88589a00b78
Pavan-Saketh-Mandha <pavansaketh97@gmail.com>
2021-06-23 16:53:20 +0530

    USE CASE 1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,22 @@
+/**
+ * Java Program to simulates a Snake and Ladder Game. The Player
+   starts from 0 rolls the die to get a number between 1 to 6, finds
+   a safe place, ladder or a snake keeps doing till the winning spot
+   100 is achieved.
+ *
+ * @author: PAVAN SAKETH
+ * @since: 11.06.2021
+ */
+
+import java.util.*;
+public class SnakeAndLadder
+{
+	//System.out.println()
+	public static void main(String[] args) 
+	{
+		System.out.println("WELCOME - Snake And Ladder - SINGLE player");
+		int start_position = 0;
+		System.out.println("Player is at position: "  + start_position);
+	
+	}
+}
